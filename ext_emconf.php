50c00ca640181e975b55f41a194882ac9682210c
Thomas Kieslich <post@thomaskieslich.de>
2017-02-14 12:24:08 +0100

    update composer for testing

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,27 +1,20 @@
 <?php
 $EM_CONF[$_EXTKEY] = array(
-    'title'        => 'Extend Textmedia',
-    'category'     => 'fe',
-    'state'        => 'stable',
-    'author'       => 'ThomasK',
+    'title' => 'Extend Textmedia',
+    'category' => 'fe',
+    'state' => 'stable',
+    'author' => 'ThomasK',
     'author_email' => 'post@thomaskieslich.de',
-    'version'      => '7.6.0',
-    'constraints'  => array(
-        'depends'   => array(
-            'typo3' => '7.6.11-8.9.99'
+    'version' => '8.5.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '8.5.1-8.7.99'
         ),
         'conflicts' => array(
             'css_styled_content' => ''
         ),
-        'suggests'  => array(
+        'suggests' => array(
             'fluid_styled_content' => ''
         ),
-    ),
-    'autoload' =>
-        array(
-            'psr-4' =>
-                array(
-                    "ThomasK\\Tkmedia\\" => "Classes"
-                )
-        )
+    )
 );
