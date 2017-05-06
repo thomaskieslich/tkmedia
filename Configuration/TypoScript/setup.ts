07ebb2c69e373b4c473a251aa096dc155f5bf7c0
Thomas Kieslich <post@thomaskieslich.de>
2017-05-06 16:39:26 +0200

    add column prefixes

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -33,12 +33,17 @@ lib.math {
 
 tt_content.textmedia {
   settings {
+    columnPrefix {
+      medium = medium-
+    }
+
     srcset {
       small = {$styles.content.textmedia.image.small}
       medium = {$styles.content.textmedia.image.medium}
       large = {$styles.content.textmedia.image.large}
     }
-    sizes{
+
+    sizes {
       medium = 40em
       test = (min-width: 40em) {dimensions.width}px, 100vw
     }
