a8ec1efef76042105ada62547ca7cf0949383cee
Thomas Kieslich <post@thomaskieslich.de>
2017-05-10 09:33:31 +0200

    fix PageTS path

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -4,6 +4,6 @@ defined('TYPO3_MODE') or die();
 // Add pageTSconfig
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
     'tkmedia',
-    'Configuration/TSconfig/Page/Page.tsconfig',
+    'Configuration/PageTS/Page.tsconfig',
     'Tkmedia'
 );
